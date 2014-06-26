799ed42f8619f615b3ae46a523bd01df3a02bce3
Jordan Hudgens <jordan@wow.com>
2014-06-26 05:42:31 -0500

    integrated updates to the form and show fields

--- madApp.js
+++ madApp.js
@@ -1,12 +1,38 @@
-app = angular.module('madApp', []);
-app.controller('MyCtrl', function($scope){
-	// Sets the story_toggle to false so it doesn't show up by default
-	$scope.story_toggle = false;
+angular.module('madApp', [])
 
-	$scope.show = function() {
-		// Make sure to set the variable!
-		$scope.story_toggle = !$scope.story_toggle; 
-	}
-	
-	console.log("test ", $scope.female_name);
-});
+	//add a controller to hold a model w/words for madlib
+	.controller('MyCtrl', function($scope) {
+
+		//initialize 
+		$scope.generatedLib = false;
+		$scope.submitted = false;
+
+		$scope.submit = function() {
+			if($scope.madLibForm.$valid) {
+				console.log('The form is valid');
+				$scope.generatedLib = true;
+			} else {
+				console.log('The form is invalid');
+				$scope.generatedLib = false;
+			}
+		};
+
+		//reset form
+		$scope.reset = function() {
+
+			$scope.madLibForm.$setPristine();
+
+			$scope.data.maleName = "";
+			$scope.data.cityName ="";
+			$scope.data.foodType = "";
+			$scope.data.adjective = "";
+
+			$scope.data.strangeLocation = "";
+			$scope.data.fictionalCharacter = "";
+			$scope.data.number = "";
+			$scope.data.color = "";
+
+			$scope.submitted = false;
+			$scope.generatedLib = false;
+		}
+	});
